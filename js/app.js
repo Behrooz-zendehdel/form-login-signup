9d22f3503feb3ae328bd90ed4262eae7ee82d2ef
behrooz <behrooz.zendedel1992@gmail.com>
2022-02-19 11:28:10 +0330

    update finished tags #login & #register

--- js/app.js
+++ js/app.js
@@ -3,11 +3,13 @@ const reg = document.querySelector('#register')
 const btn = document.querySelector('#btn')
 
 function login() {
-
+    log.style.right = '0'
+    reg.style.right = '450px'
+    btn.style.right = '0'
 }
 
 function register() {
-    log.style.right = '450px'
+    log.style.right = '-450px'
     reg.style.right = '0'
     btn.style.right = '110px'
 }
