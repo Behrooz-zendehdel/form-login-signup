0ba0d67161eb85b4f27655df6ac2fdf3c3d66002
behrooz <behrooz.zendedel1992@gmail.com>
2022-02-19 11:17:31 +0330

    add file js & update tags

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const log = document.querySelector('#login')
+const reg = document.querySelector('#register')
+const btn = document.querySelector('#btn')
+
+function login() {
+
+}
+
+function register() {
+    log.style.right = '450px'
+    reg.style.right = '0'
+    btn.style.right = '110px'
+}
